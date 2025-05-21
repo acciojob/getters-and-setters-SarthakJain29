d41513741d781c8eb1b047a13d6be544a84f5ae4
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-05-21 13:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	Person(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	get name(){
+		return this.name;
+	}
+	get age(){
+		return this.age;
+	}
+	set age(newAge){
+		this.age = newAge;
+	}
+	set name(newName){
+		this.name = newName;
+	}
+}
+	
+class Student extends Person {
+	void study(){
+		console.log(this.name + " is studying");
+	}
+}
 
-class Student extends Person {}
-
-class Teacher extends Person {}
+class Teacher extends Person {
+	void teach(){
+		console.log(this.name + " is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
